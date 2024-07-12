63422dfe289965944676f914143f50002bca43ab
Kubilay Kanoglu <mangament.kr1pton@gmx.de>
2024-07-12 18:49:27 +0200

    New Comments added to the Code

--- Webscraper.py
+++ Webscraper.py
@@ -16,24 +16,24 @@ def Agent(url, filename):
 def prettify_html(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         soup = BeautifulSoup(file, 'html.parser')
-        return soup  # Return the BeautifulSoup object
+        return soup  #Return the BeautifulSoup object
 
 def shorten_text(text):
     shortened_text = text.split(' (')[0] #shortens info
-    return shortened_text.strip()
+    return shortened_text.strip() #returns the info
 
 def country_infos(soup, filename):
-    table = soup.find('table')  # Assuming the data is within a <table> element
+    table = soup.find('table')  #Assuming the data is within a <table> element
     if table:
         with open(filename, 'w', encoding='utf-8') as file:
-            rows = table.find_all('tr')
+            rows = table.find_all('tr') #finds all tr data
             for row in rows:
                 columns = row.find_all(['th', 'td'])  # Find all the th and td data
                 if columns:
                     for col in columns:
                         cleaned_text = shorten_text(col.text.strip())
-                        file.write(f"{cleaned_text:<30} ")  # Write each column's text, stripped and formatted
-                    file.write("\n")  # Move to the next line after writing all columns for a row
+                        file.write(f"{cleaned_text:<30} ")  #Write each column's text, stripped and formatted
+                    file.write("\n")  #Move to the next line after writing all columns for a row
     else:
         print("No table found in the HTML.")
 
