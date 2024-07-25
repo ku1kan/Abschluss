b4735bd7e46c78e60ce5d40ae203e0f9a38ede51
Kubilay Kanoglu <mangament.kr1pton@gmx.de>
2024-07-25 21:47:22 +0200

    Used for Gui, furhter use case comming

---
+++ Countries_info.py
@@ -0,0 +1,314 @@
+countries = [
+    {
+        "Country": "Germany",
+        "Population": 83251296,
+        "Capital": "Berlin",
+        "Area": 357580,
+        "Subdivisions": [
+            {
+                "Name": "Hamburg",
+                "Population": 3576873,
+                "Capital": "Hamburg",
+                "Area": 891
+            },
+            {
+                "Name": "Bavaria",
+                "Population": 13435062,
+                "Capital": "Munich",
+                "Area": 70541
+            },
+            {
+                "Name": "North Rhine-Westphalia",
+                "Population": 17932529,
+                "Capital": "Düsseldorf",
+                "Area": 34112
+            }
+        ]
+    },
+    {
+        "Country": "France",
+        "Population": 64888518,
+        "Capital": "Paris",
+        "Area": 549087,
+        "Subdivisions": [
+            {
+                "Name": "Auvergne-Rhône-Alpes",
+                "Population": 12278263,
+                "Capital": "Lyon",
+                "Area": 12012
+            },
+            {
+                "Name": "Provence-Alpes-Côte d'Azur",
+                "Population": 8235923,
+                "Capital": "Marseille",
+                "Area": 69711
+            },
+            {
+                "Name": "Nouvelle-Aquitaine",
+                "Population": 5059117,
+                "Capital": "Bordeaux",
+                "Area": 31400
+            }
+        ]
+    },
+    {
+        "Country": "Japan",
+        "Population": 122596148,
+        "Capital": "Tokyo",
+        "Area": 377975,
+        "Subdivisions": [
+            {
+                "Name": "Tokyo Metropolis",
+                "Population": 37115035,
+                "Capital": "Tokyo",
+                "Area": 2194
+            },
+            {
+                "Name": "Osaka Prefecture",
+                "Population": 18411322,
+                "Capital": "Osaka",
+                "Area": 225
+            },
+            {
+                "Name": "Aichi Prefecture",
+                "Population": 2610000,
+                "Capital": "Nagoya",
+                "Area": 461
+            }
+        ]
+    },
+    {
+        "Country": "Brazil",
+        "Population": 217702343,
+        "Capital": "Brasília",
+        "Area": 8514877,
+        "Subdivisions": [
+            {
+                "Name": "São Paulo State",
+                "Population": 22806704,
+                "Capital": "São Paulo",
+                "Area": 1521
+            },
+            {
+                "Name": "Rio de Janeiro State",
+                "Population": 13824347,
+                "Capital": "Rio de Janeiro",
+                "Area": 1221
+            },
+            {
+                "Name": "Minas Gerais",
+                "Population": 21040000,
+                "Capital": "Belo Horizonte",
+                "Area": 586528
+            }
+        ]
+    },
+    {
+        "Country": "Mexico",
+        "Population": 129438473,
+        "Capital": "Mexico City",
+        "Area": 1964375,
+        "Subdivisions": [
+            {
+                "Name": "Mexico City",
+                "Population": 22505315,
+                "Capital": "Mexico City",
+                "Area": 1495
+            },
+            {
+                "Name": "State of Mexico",
+                "Population": 17427790,
+                "Capital": "Toluca de Lerdo",
+                "Area": 22351
+            },
+            {
+                "Name": "Jalisco",
+                "Population": 8730000,
+                "Capital": "Guadalajara",
+                "Area": 78588
+            }
+        ]
+    },
+    {
+        "Country": "Canada",
+        "Population": 39124521,
+        "Capital": "Ottawa",
+        "Area": 9984670,
+        "Subdivisions": [
+            {
+                "Name": "Ontario",
+                "Population": 15262660,
+                "Capital": "Toronto",
+                "Area": 1076395
+            },
+            {
+                "Name": "Quebec",
+                "Population": 8948540,
+                "Capital": "Quebec City",
+                "Area": 1542056
+            },
+            {
+                "Name": "British Columbia",
+                "Population": 5458000,
+                "Capital": "Victoria",
+                "Area": 944735
+            }
+        ]
+    },
+    {
+        "Country": "Australia",
+        "Population": 26713464,
+        "Capital": "Canberra",
+        "Area": 7692024,
+        "Subdivisions": [
+            {
+                "Name": "New South Wales",
+                "Population": 8336369,
+                "Capital": "Sydney",
+                "Area": 800642
+            },
+            {
+                "Name": "Victoria",
+                "Population": 6809973,
+                "Capital": "Melbourne",
+                "Area": 237659
+            },
+            {
+                "Name": "Queensland",
+                "Population": 5093000,
+                "Capital": "Brisbane",
+                "Area": 1852642
+            }
+        ]
+    },
+    {
+        "Country": "India",
+        "Population": 1442423033,
+        "Capital": "New Delhi",
+        "Area": 3287263,
+        "Subdivisions": [
+            {
+                "Name": "Uttar Pradesh",
+                "Population": 229239237,
+                "Capital": "Lucknow",
+                "Area": 240286
+            },
+            {
+                "Name": "Maharashtra",
+                "Population": 127684000,
+                "Capital": "Mumbai",
+                "Area": 307713
+            },
+            {
+                "Name": "West Bengal",
+                "Population": 101829000,
+                "Capital": "Kolkata",
+                "Area": 88752
+            }
+        ]
+    },
+    {
+        "Country": "Russia",
+        "Population": 143931070,
+        "Capital": "Moscow",
+        "Area": 17098242,
+        "Subdivisions": [
+            {
+                "Name": "Moscow City",
+                "Population": 12712305,
+                "Capital": "Moscow",
+                "Area": 2561
+            },
+            {
+                "Name": "Saint Petersburg",
+                "Population": 5581707,
+                "Capital": "Saint Petersburg",
+                "Area": 1439
+            },
+            {
+                "Name": "Krasnoyarsk Krai",
+                "Population": 2861000,
+                "Capital": "Krasnoyarsk",
+                "Area": 2370000
+            }
+        ]
+    },
+    {
+        "Country": "Turkey",
+        "Population": 86284246,
+        "Capital": "Ankara",
+        "Area": 785350,
+        "Subdivisions": [
+            {
+                "Name": "Istanbul",
+                "Population": 16047350,
+                "Capital": "Istanbul",
+                "Area": 5343
+            },
+            {
+                "Name": "Ankara",
+                "Population": 5477087,
+                "Capital": "Ankara",
+                "Area": 25631
+            },
+            {
+                "Name": "Izmir",
+                "Population": 4367250,
+                "Capital": "Izmir",
+                "Area": 7340
+            }
+        ]
+    },
+    {
+        "Country": "Italy",
+        "Population": 58688498,
+        "Capital": "Rome",
+        "Area": 301340,
+        "Subdivisions": [
+            {
+                "Name": "Lazio",
+                "Population": 5720272,
+                "Capital": "Rome",
+                "Area": 17232
+            },
+            {
+                "Name": "Lombardy",
+                "Population": 10006000,
+                "Capital": "Milan",
+                "Area": 23863
+            },
+            {
+                "Name": "Campania",
+                "Population": 5811000,
+                "Capital": "Naples",
+                "Area": 13590
+            }
+        ]
+    },
+    {
+        "Country": "South Korea",
+        "Population": 51780579,
+        "Capital": "Seoul",
+        "Area": 100032,
+        "Subdivisions": [
+            {
+                "Name": "Seoul",
+                "Population": 9860000,
+                "Capital": "Seoul",
+                "Area": 605
+            },
+            {
+                "Name": "Daegu",
+                "Population": 3400000,
+                "Capital": "Daegu",
+                "Area": 770
+            },
+            {
+                "Name": "Gwangju",
+                "Population": 3000000,
+                "Capital": "Gwangju",
+                "Area": 1029
+            }
+        ]
+    }
+]
