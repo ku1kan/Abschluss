8235b9a313706e8cf4bbcac186a971c738634dc6
Kubilay Kanoglu <mangament.kr1pton@gmx.de>
2024-08-04 20:31:23 +0200

    now handles float numbers for the area

--- Countries_info.py
+++ Countries_info.py
@@ -3,25 +3,25 @@ countries = [
         "Country": "Germany",
         "Population": 83251296,
         "Capital": "Berlin",
-        "Area": 357580,
+        "Area": 357580.0,
         "Subdivisions": [
             {
                 "Name": "Hamburg",
                 "Population": 3576873,
                 "Capital": "Hamburg",
-                "Area": 891
+                "Area": 891.0
             },
             {
                 "Name": "Bavaria",
                 "Population": 13435062,
                 "Capital": "Munich",
-                "Area": 70541
+                "Area": 70541.0
             },
             {
                 "Name": "North Rhine-Westphalia",
                 "Population": 17932529,
                 "Capital": "Düsseldorf",
-                "Area": 34112
+                "Area": 34112.0
             }
         ]
     },
@@ -29,25 +29,25 @@ countries = [
         "Country": "France",
         "Population": 64888518,
         "Capital": "Paris",
-        "Area": 549087,
+        "Area": 549087.0,
         "Subdivisions": [
             {
                 "Name": "Auvergne-Rhône-Alpes",
                 "Population": 12278263,
                 "Capital": "Lyon",
-                "Area": 12012
+                "Area": 12012.0
             },
             {
                 "Name": "Provence-Alpes-Côte d'Azur",
                 "Population": 8235923,
                 "Capital": "Marseille",
-                "Area": 69711
+                "Area": 69711.0
             },
             {
                 "Name": "Nouvelle-Aquitaine",
                 "Population": 5059117,
                 "Capital": "Bordeaux",
-                "Area": 31400
+                "Area": 31400.0
             }
         ]
     },
@@ -55,25 +55,25 @@ countries = [
         "Country": "Japan",
         "Population": 122596148,
         "Capital": "Tokyo",
-        "Area": 377975,
+        "Area": 377975.0,
         "Subdivisions": [
             {
                 "Name": "Tokyo Metropolis",
                 "Population": 37115035,
                 "Capital": "Tokyo",
-                "Area": 2194
+                "Area": 2194.0
             },
             {
                 "Name": "Osaka Prefecture",
                 "Population": 18411322,
                 "Capital": "Osaka",
-                "Area": 225
+                "Area": 225.0
             },
             {
                 "Name": "Aichi Prefecture",
                 "Population": 2610000,
                 "Capital": "Nagoya",
-                "Area": 461
+                "Area": 461.0
             }
         ]
     },
@@ -81,25 +81,25 @@ countries = [
         "Country": "Brazil",
         "Population": 217702343,
         "Capital": "Brasília",
-        "Area": 8514877,
+        "Area": 8514877.0,
         "Subdivisions": [
             {
                 "Name": "São Paulo State",
                 "Population": 22806704,
                 "Capital": "São Paulo",
-                "Area": 1521
+                "Area": 1521.0
             },
             {
                 "Name": "Rio de Janeiro State",
                 "Population": 13824347,
                 "Capital": "Rio de Janeiro",
-                "Area": 1221
+                "Area": 1221.0
             },
             {
                 "Name": "Minas Gerais",
                 "Population": 21040000,
                 "Capital": "Belo Horizonte",
-                "Area": 586528
+                "Area": 586528.0
             }
         ]
     },
@@ -107,25 +107,25 @@ countries = [
         "Country": "Mexico",
         "Population": 129438473,
         "Capital": "Mexico City",
-        "Area": 1964375,
+        "Area": 1964375.0,
         "Subdivisions": [
             {
                 "Name": "Mexico City",
                 "Population": 22505315,
                 "Capital": "Mexico City",
-                "Area": 1495
+                "Area": 1495.0
             },
             {
                 "Name": "State of Mexico",
                 "Population": 17427790,
                 "Capital": "Toluca de Lerdo",
-                "Area": 22351
+                "Area": 22351.0
             },
             {
                 "Name": "Jalisco",
                 "Population": 8730000,
                 "Capital": "Guadalajara",
-                "Area": 78588
+                "Area": 78588.0
             }
         ]
     },
@@ -133,25 +133,25 @@ countries = [
         "Country": "Canada",
         "Population": 39124521,
         "Capital": "Ottawa",
-        "Area": 9984670,
+        "Area": 9984670.0,
         "Subdivisions": [
             {
                 "Name": "Ontario",
                 "Population": 15262660,
                 "Capital": "Toronto",
-                "Area": 1076395
+                "Area": 1076395.0
             },
             {
                 "Name": "Quebec",
                 "Population": 8948540,
                 "Capital": "Quebec City",
-                "Area": 1542056
+                "Area": 1542056.0
             },
             {
                 "Name": "British Columbia",
                 "Population": 5458000,
                 "Capital": "Victoria",
-                "Area": 944735
+                "Area": 944735.0
             }
         ]
     },
@@ -159,25 +159,25 @@ countries = [
         "Country": "Australia",
         "Population": 26713464,
         "Capital": "Canberra",
-        "Area": 7692024,
+        "Area": 7692024.0,
         "Subdivisions": [
             {
                 "Name": "New South Wales",
                 "Population": 8336369,
                 "Capital": "Sydney",
-                "Area": 800642
+                "Area": 800642.0
             },
             {
                 "Name": "Victoria",
                 "Population": 6809973,
                 "Capital": "Melbourne",
-                "Area": 237659
+                "Area": 237659.0
             },
             {
                 "Name": "Queensland",
                 "Population": 5093000,
                 "Capital": "Brisbane",
-                "Area": 1852642
+                "Area": 1852642.0
             }
         ]
     },
@@ -185,25 +185,25 @@ countries = [
         "Country": "India",
         "Population": 1442423033,
         "Capital": "New Delhi",
-        "Area": 3287263,
+        "Area": 3287263.0,
         "Subdivisions": [
             {
                 "Name": "Uttar Pradesh",
                 "Population": 229239237,
                 "Capital": "Lucknow",
-                "Area": 240286
+                "Area": 240286.0
             },
             {
                 "Name": "Maharashtra",
                 "Population": 127684000,
                 "Capital": "Mumbai",
-                "Area": 307713
+                "Area": 307713.0
             },
             {
                 "Name": "West Bengal",
                 "Population": 101829000,
                 "Capital": "Kolkata",
-                "Area": 88752
+                "Area": 88752.0
             }
         ]
     },
@@ -211,25 +211,25 @@ countries = [
         "Country": "Russia",
         "Population": 143931070,
         "Capital": "Moscow",
-        "Area": 17098242,
+        "Area": 17098242.0,
         "Subdivisions": [
             {
                 "Name": "Moscow City",
                 "Population": 12712305,
                 "Capital": "Moscow",
-                "Area": 2561
+                "Area": 2561.0
             },
             {
                 "Name": "Saint Petersburg",
                 "Population": 5581707,
                 "Capital": "Saint Petersburg",
-                "Area": 1439
+                "Area": 1439.0
             },
             {
                 "Name": "Krasnoyarsk Krai",
                 "Population": 2861000,
                 "Capital": "Krasnoyarsk",
-                "Area": 2370000
+                "Area": 2370000.0
             }
         ]
     },
@@ -237,25 +237,25 @@ countries = [
         "Country": "Turkey",
         "Population": 86284246,
         "Capital": "Ankara",
-        "Area": 785350,
+        "Area": 785350.0,
         "Subdivisions": [
             {
                 "Name": "Istanbul",
                 "Population": 16047350,
                 "Capital": "Istanbul",
-                "Area": 5343
+                "Area": 5343.0
             },
             {
                 "Name": "Ankara",
                 "Population": 5477087,
                 "Capital": "Ankara",
-                "Area": 25631
+                "Area": 25631.0
             },
             {
                 "Name": "Izmir",
                 "Population": 4367250,
                 "Capital": "Izmir",
-                "Area": 7340
+                "Area": 7340.0
             }
         ]
     },
@@ -263,25 +263,25 @@ countries = [
         "Country": "Italy",
         "Population": 58688498,
         "Capital": "Rome",
-        "Area": 301340,
+        "Area": 301340.0,
         "Subdivisions": [
             {
                 "Name": "Lazio",
                 "Population": 5720272,
                 "Capital": "Rome",
-                "Area": 17232
+                "Area": 17232.0
             },
             {
                 "Name": "Lombardy",
                 "Population": 10006000,
                 "Capital": "Milan",
-                "Area": 23863
+                "Area": 23863.0
             },
             {
                 "Name": "Campania",
                 "Population": 5811000,
                 "Capital": "Naples",
-                "Area": 13590
+                "Area": 13590.0
             }
         ]
     },
@@ -289,25 +289,25 @@ countries = [
         "Country": "South Korea",
         "Population": 51780579,
         "Capital": "Seoul",
-        "Area": 100032,
+        "Area": 100032.0,
         "Subdivisions": [
             {
                 "Name": "Seoul",
                 "Population": 9860000,
                 "Capital": "Seoul",
-                "Area": 605
+                "Area": 605.0
             },
             {
                 "Name": "Daegu",
                 "Population": 3400000,
                 "Capital": "Daegu",
-                "Area": 770
+                "Area": 770.0
             },
             {
                 "Name": "Gwangju",
                 "Population": 3000000,
                 "Capital": "Gwangju",
-                "Area": 1029
+                "Area": 1029.0
             }
         ]
     }
